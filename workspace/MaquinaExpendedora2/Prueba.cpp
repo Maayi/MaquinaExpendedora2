330c9acd87c7f64e17ebe64ad69c82c3f931d0b7
anneidigoras <anneidigoras@opendeusto.es>
2017-02-17 22:21:01 +0100

    Primera Pversion

---
+++ workspace/MaquinaExpendedora2/Prueba.cpp
@@ -0,0 +1,18 @@
+/*
+ * Prueba.cpp
+ *
+ *  Created on: 17 feb. 2017
+ *      Author: Anne
+ */
+
+#include "Prueba.h"
+
+Prueba::Prueba() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Prueba::~Prueba() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ workspace/MaquinaExpendedora2/Prueba.h
@@ -0,0 +1,17 @@
+/*
+ * Prueba.h
+ *
+ *  Created on: 17 feb. 2017
+ *      Author: Anne
+ */
+
+#ifndef PRUEBA_H_
+#define PRUEBA_H_
+
+class Prueba {
+public:
+	Prueba();
+	virtual ~Prueba();
+};
+
+#endif /* PRUEBA_H_ */
