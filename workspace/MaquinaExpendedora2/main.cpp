47ab90cbb71b9c7a9ddc2f5f8c962318a10609ec
Maayi <mayi.echeveste@opendeusto.es>
2017-05-13 19:00:11 +0200

    Sobrecarga de operadores, concretamente el de escritura para sacar alimentos por pantalla

--- workspace/MaquinaExpendedora2/main.cpp
+++ workspace/MaquinaExpendedora2/main.cpp
@@ -4,6 +4,28 @@
 #include <iostream>
 using namespace std;
 
+ostream& operator<< (ostream &out, const Producto &p)
+{
+	out << p.getID()<< ": "<< p.getNombre() <<  '\t' <<"Precio: " << p.getprecioP() << " Stock" << p.getNum()<< endl;
+	
+}
+
+ostream& operator<< (ostream &out, const Alimento &a) //: Producto (stream &out, const Alimento &a )
+{
+	out << a.getID()<< ": "<< a.getNombre() <<  '\t' <<"Precio: " << a.getprecioP() <<endl;
+	out << '\t' <<'\t' <<"Stock: " << a.getNum()<< endl;
+	out <<'\t' <<'\t' <<"Gramos: " << a.getGramos()<< endl;
+	
+}
+/*ostream& operator<< (ostream &out, const Bebida &b)
+{
+	out << "Capacidad= " << a.getCapacity() << '\t' <<"Valores: " << endl;
+	for (int i =0; i< a.getCapacity(); i++)
+	{
+		out << a.getValue(i) << endl;
+	}
+}
+*/
 int main ()
 {
 
@@ -12,10 +34,12 @@ int main ()
 	cout << "Producto : "<<p1.getNum() <<endl;*/ // esto ya no se puede hacer por que ahora la clase persona es abstracta
 
 	Alimento a1 (2, "Lorea", 2.0,15, 33);
-	cout << "Alimento : "<< a1.getGramos() << endl;
+	//cout << "Alimento : "<< a1.getGramos() << endl;
+	cout<< a1;
 
 	Bebida b1 (2, "Marcos", 5.0, 5, 100, "cucu");
-	b1.setMiliL(200);
+	//b1.setMiliL(200);
+
 
 	cout << "Bebida : "<< b1.getMiliL() << endl;
 
