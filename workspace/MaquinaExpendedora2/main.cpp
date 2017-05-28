403c5770140091247b64842b00e923089eac62bf
Maayi <mayi.echeveste@opendeusto.es>
2017-05-28 22:42:56 +0200

    Solucionado problema vision compras

--- workspace/MaquinaExpendedora2/main.cpp
+++ workspace/MaquinaExpendedora2/main.cpp
@@ -282,24 +282,13 @@ void LeerCompras()
 
           istringstream iss(compra);
           string s;
-          while ( getline( iss, s, ' ' ) ) 
+          while ( getline( iss, s, ' ' )) 
           {
+            
+              
               atributos.push_back(s);
           }
-          if(atributos.size()<5)
-          {
 
-          dniU = atributos[0].c_str();
-          nomU=atributos[1].c_str();
-          idP = atoi( atributos[2].c_str());
-          nombreP = atributos[3].c_str();
-          
-          Compra c (dniU, nomU, idP, nombreP);
-          cout<<'\t'<< '\t'<< c;
-          VectCompras.push_back(c);
-          }
-          else if (atributos.size()<6) 
-          {
             
           dniU = atributos[0].c_str();
           nomU=atributos[1].c_str();
@@ -311,8 +300,6 @@ void LeerCompras()
           Compra c (dniU, nomU, idP, nombreP);
           cout<<'\t'<< '\t'<< c;
           VectCompras.push_back(c);
-          }
-          else cout<< "Compras MAL REGISTRADAS"<< endl;
 
           atributos.clear();
                     
